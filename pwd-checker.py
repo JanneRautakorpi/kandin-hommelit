239b4ce8923218fb10d6c166ac61bdda45337f14
Janne <janne.rautakorpi@netikka.fi>
2024-10-09 00:15:38 +0300

    removed global variables. Dont like them

--- pwd-checker.py
+++ pwd-checker.py
@@ -1,17 +1,6 @@
 import argparse
 from math import log2
 
-'''
-For now, we will use global constants for character counts. 
-TO DO: change it to be asked form the user as an argument.
-'''
-CAPITAL_LETTERS = 26                                        # Capital letters in English alphabet
-LOWER_CASE_LETTERS = 26                                     # Lower case letters in English alphabet
-TOTAL_LETTERS = CAPITAL_LETTERS + LOWER_CASE_LETTERS        # self-explanatory :D
-NUMBERS = 10                                                # integers from 0 to 9
-TOTAL_CHARS = TOTAL_LETTERS + NUMBERS
-REQ_LENGTH = 12                                             # required length for the password (used for entropy calcs)
-
 def main():
     print("This script assumes you use:")
     print("- 52 letters (English alphabet in lower and caps)")
@@ -24,7 +13,7 @@ def main():
     
     passwd = args.password
     length = len(passwd)
-    entropy = calculateEntropy(REQ_LENGTH, TOTAL_CHARS)
+    entropy = calculateEntropy(8, (26 + 26 + 10 + 32))
     print(entropy)
     containsNumber = containNumber(passwd)
     containsLowerCase = containLowerCase(passwd)

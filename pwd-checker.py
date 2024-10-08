ded8dc3dfbf65b46c7e5487397ef3b4a7a9212a4
Janne <janne.rautakorpi@netikka.fi>
2024-10-08 13:45:31 +0300

    implementation of entropy function

--- pwd-checker.py
+++ pwd-checker.py
@@ -1,19 +1,30 @@
 import argparse
+from math import log2
 
 def main():
     print("This script assumes you use:")
     print("- 26 letters (English alphabet)")
     print("- numbers from 0 to 9")
     print()
-    
+
     parser = argparse.ArgumentParser()
     parser.add_argument("password", type=str, help="Password to be checked.")
     args = parser.parse_args()
     
     passwd = args.password
     length = len(passwd)
-    print(length)
 
+def calculateEntropy(L, R):
+    '''
+    Calculates entropy.
+
+        Parameters:
+            L (int): the required length for the password
+            R (int): possible characters
+    '''
+
+    E = log2(R**L)
+    return E
 
 if __name__=='__main__':
     main()

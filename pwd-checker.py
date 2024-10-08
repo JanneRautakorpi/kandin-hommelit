af299012860c0a29a7b99ed852174ecc98fe46c0
Janne <janne.rautakorpi@netikka.fi>
2024-10-08 13:33:10 +0300

    intro text

--- pwd-checker.py
+++ pwd-checker.py
@@ -1,6 +1,11 @@
 import argparse
 
 def main():
+    print("This script assumes you use:")
+    print("- 26 letters (English alphabet)")
+    print("- numbers from 0 to 9")
+    print()
+    
     parser = argparse.ArgumentParser()
     parser.add_argument("password", type=str, help="Password to be checked.")
     args = parser.parse_args()

10e6bc17c0d497473026c4cc8407d3afba393c89
Janne <janne.rautakorpi@netikka.fi>
2024-11-15 14:29:57 +0200

    Added argument for password count

--- scripts/pwd-picker.py
+++ scripts/pwd-picker.py
@@ -5,10 +5,13 @@ def randomPick():
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", type=str, help="Input file.", required=True)
     parser.add_argument("--output", type=str, help="Output file", required=False)
+    parser.add_argument("--count", type=int, help="How many passwords to pick", required=True)
     args = parser.parse_args()
 
     iFile = args.input
     oFile = args.output
+    count = args.count
+
     print(iFile)
     try:
         with open(iFile, "r") as f:
@@ -18,7 +21,7 @@ def randomPick():
         print(e)
         return
 
-    for _ in range(0, 8):
+    for _ in range(0, count):
         print(random.choice(lines))
     return
 
